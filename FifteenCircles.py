9737080156bc641109041e2c350d20ba1c9461ad
sun4geek <sun4geek@gmail.com>
2017-05-31 16:24:47 +0900

    [Add] ファイルの追加
    すべてを実装した。

---
+++ FifteenCircles.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+def fifteenCircles():
+	print("input the number -> ", end = " ")
+	target = int(input())
+	print("Calculating...")
+	for bottomFirst in range(target):
+		for bottomSecond in range(target):
+			for bottomThird in range(target):
+				for bottomFourth in range(target):
+					for bottomFifth in range(target):
+						firstLine = [bottomFirst, bottomSecond, bottomThird, bottomFourth, bottomFifth]
+						secondLine = [bottomFirst + bottomSecond, bottomSecond + bottomThird, bottomThird + bottomFourth, bottomFourth + bottomFifth]
+						thirdLine = [secondLine[0] + secondLine[1], secondLine[1] + secondLine[2], secondLine[2] + secondLine[3]]
+						fourthLine = [thirdLine[0] + thirdLine[1], thirdLine[1] + thirdLine[2]]
+						result = fourthLine[0] + fourthLine[1]
+						if(result != target):
+							firstLine = []
+							secondLine = []
+							thirdLine = []
+							fourthLine = []
+						else:
+							countFirst = Counter(firstLine)
+							countSecond = Counter(secondLine)
+							countThird = Counter(thirdLine)
+							countFourth = Counter(fourthLine)
+							counter = countFirst + countSecond + countThird + countFourth
+							if(len(counter) == 14):
+								print("result:")
+								print(str(result))
+								print(fourthLine[0], fourthLine[1])
+								print(thirdLine[0], thirdLine[1], thirdLine[2])
+								print(secondLine[0], secondLine[1], secondLine[2], secondLine[3])
+								print(firstLine[0], firstLine[1], firstLine[2], firstLine[3], firstLine[4])
+								print("finish")
+								return
+							else:
+								countFirst = ()
+								countSecond = ()
+								countThird = ()
+								countFourth = ()
+								couner = Counter()
+	print("false")
+
+def main():
+	fifteenCircles()
+
+if __name__ == '__main__':
+	main()
